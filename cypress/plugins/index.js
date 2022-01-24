140697c5d9e9f35fc5407b08ac80949b142190a8
DorijanH <dhaspl@gmail.com>
2022-01-24 14:16:29 +0100

    Added component tests and updated CI GH action

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -14,10 +14,15 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+const injectedDevServer = require('@cypress/react/plugins/next');
+
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  injectedDevServer(on, config);
+
+  return config;
 };
