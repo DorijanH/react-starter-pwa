be1e72ee588542c43cdb775d2c5d75a396503af7
DorijanH <dhaspl@gmail.com>
2022-02-08 00:18:15 +0100

    Added Roboto font DN

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -61,6 +61,7 @@ class CustomDocument extends Document {
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
 
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
           <link rel="apple-touch-icon" href="assets/icons/icon-192x192.png" />
           <link rel="icon" href="/favicon.ico" />
           <link rel="manifest" href="/manifest.json" />
